9914123ea5870cbd6d232c1b81610638ad934e51
maiconroch06 <maiconpablo588@gmail.com>
2026-08-21 09:13:08 -0300

    Organização dos arquivos

---
+++ class/Adress.py
@@ -0,0 +1,11 @@
+class Adress:
+    def __init__(self, id, cep, cidade,estado,logradouro,numero,bairro,sequencia,complemento):
+        self.cep = cep
+        self.cidade = cidade
+        self.estado = estado
+        self.logradouro = logradouro
+        self.numero = numero
+        self.bairro = bairro
+        self.sequencia = sequencia
+        self.complemento = complemento
+        self.id = id
---
+++ class/__init__.py
@@ -0,0 +1,10 @@
+
+# o que vem depois do from é o arquivo/caminho e o que vem após o import é a classe (o que esta dentro do arquivo Adress.py, por exemplo)
+from .Adress import Adress
+from .Users import Users
+from .Enterprise import Enterprise
+
+
+# Então poderá fazer: rom class import Adress, em vez de [ from class.Adress import Adress ]
+
+#. significa "da mesma pasta/pacote".
